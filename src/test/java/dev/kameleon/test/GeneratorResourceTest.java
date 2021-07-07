92db82d7aeda8769e2882941029049fc677f6586
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-07-07 13:10:46 -0400

    Test generator stub

---
+++ src/test/java/dev/kameleon/test/GeneratorResourceTest.java
@@ -0,0 +1,14 @@
+package dev.kameleon.test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+
+@QuarkusTest
+public class GeneratorResourceTest {
+
+    @Test
+    public void testGenerator() {
+        // TODO
+    }
+}
